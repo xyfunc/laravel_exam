f50f2620a6165dbf5f6c8450ae3f5ea8d770f777
beechen <sw_chenyuzhe@163.com>
2017-03-04 10:15:35 +0800

    test vue

--- src/resources/views/html/index.php
+++ src/resources/views/html/index.php
@@ -3,8 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <script type="text/javascript" src="../js/vue.js"></script>
 </head>
 <body>
 <h1>hello world</h1>
+
+<div class="container">
+    <div class="content">
+        <div class="title">
+            <p>@{{ message }}</p>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    new Vue({
+        el: '.title',
+        data: {
+            message: 'Hello Laravel!'
+        }
+    })
+</script>
 </body>
 </html>
